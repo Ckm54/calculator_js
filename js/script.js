633277f21f709b3e5525082d35cc4a9aefdd1f04
Collo <mutaic84@gmail.com>
2022-03-09 12:25:13 +0300

    add jquery function to handle addition functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 var add = (number1, number2) => number1 + number2;
-var num1 = parseInt(prompt("Enter the first number: "));
-var num2 = parseInt(prompt("Enter the second number: "));
-var result = add(num1, num2);
-alert(result);
+var subtract = (number1, number2) => number1 - number2;
+var multiply = (number1, number2) => number1 * number2;
+var divide = (number1, number2) => number1 / number2;
+
+
+$(document).ready(function () {
+    $("form#add").submit(function (e) { 
+        e.preventDefault();
+        var num1 = parseInt($("#add1").val());
+        var num2 = parseInt($("#add2").val());
+        var result = add(num1, num2);
+        alert(result);
+    });
+});
